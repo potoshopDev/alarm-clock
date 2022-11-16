c488707b4eff755f870b755e9edad8c3e407de5b
Oleg Belekhov <obelekhov@fogsoft.ru>
2022-11-16 16:59:08 +0300

    version 1.0a

--- build2.bat
+++ build2.bat
@@ -2,7 +2,7 @@ cd bin
 del /Q *.exe
 cd ..
 
-g++ *.cpp -o AlarmClock.exe -lstdc++ -Bdynamic -std=c++17 -s 
+g++ main.cpp -o AlarmClock.exe -lstdc++ -Bdynamic -std=c++17 -s 
 strip AlarmClock.exe
 
 copy /Y AlarmClock.exe bin
--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <ctime>
+#include <cstdlib>
+#include <Windows.h>
+
+
 
 int main()
 {
     auto  inWaitMinutes = 0;
 
-    auto printT = []() {
+    
+
+    auto printT = []() 
+    {
         printf("Enter the waiting time(m): ");
     };
+    auto printWaiting = [&inWaitMinutes]() 
+    {
+            printf("WAITING:  %d\n", inWaitMinutes);
+    };
+
+
+
     printT();
 
     while (scanf_s("%d", &inWaitMinutes) == 0)
@@ -16,9 +31,71 @@ int main()
 
     }
 
+    printWaiting();
 
+    auto StartTime = clock();
+    auto CurrentTime = clock();
 
+    auto CalculateTaiming = [&StartTime, &CurrentTime] () 
+    {
+        auto SecToMin = 60.0;
+        return  static_cast<double>((CurrentTime - StartTime)) / CLOCKS_PER_SEC / SecToMin;
 
-    printf("WAITING:  %d", inWaitMinutes);
+    };
+
+    auto PrintTimer = [&inWaitMinutes](auto CurrTimeSec)
+    {
+        const auto CurrTimeMinf = static_cast<int> (CurrTimeSec / 60);
+        const auto CurrTimeSecc = static_cast<int> (CurrTimeMinf * 60 - CurrTimeSec);
+        const auto CurrTimeSecf = CurrTimeSecc < 0 ? CurrTimeSecc * -1 : CurrTimeSecc;
+
+        auto LeftMin = inWaitMinutes;
+        auto LeftSec = 0;
+
+        LeftMin -= CurrTimeMinf;
+        LeftSec -= CurrTimeSecf;
+
+        if (LeftSec < 0)
+        {
+            LeftMin -= 1;
+            LeftSec = 60 - CurrTimeSecf;
+        }
+
+        system("cls");
+
+        printf_s("TIMER: %02d:%02d \\ %02d:00\n", LeftMin, LeftSec, inWaitMinutes);
+    };
+    
+
+    while (CalculateTaiming() <= static_cast <double> (inWaitMinutes))
+    {
+        const auto CurrTime = CalculateTaiming();
+        const auto CurrTimeSec = static_cast<int> (CurrTime * 60);
+        
+       
+
+        static auto LastSec = 0;
+        
+        if (CurrTimeSec > LastSec)
+        {
+            PrintTimer(CurrTimeSec);
+            LastSec = CurrTimeSec;
+            
+        }
+        
+        Sleep(100);
+
+        //printf_s ("min: %f", CalculateTaiming());
+        CurrentTime = clock();
+    }
+
+    const auto CurrTime = CalculateTaiming();
+    const auto CurrTimeSec = static_cast<int> (CurrTime * 60);
+    PrintTimer(CurrTimeSec);
+ 
+    system ("call 1.mp3");
+    system ("start https://www.youtube.com/watch?v=gkROXD2kzyM");
+    system("pause");
+
     return 0;
 }
